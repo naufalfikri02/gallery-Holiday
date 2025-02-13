ed5c97d6198d6d5f6e32daa82aba41716cf1ea16
naufalfikri0206 <Naufalfikri0206@gmail.com>
2025-02-13 20:25:15 +0700

    add music

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ document.querySelectorAll('.gallery-img').forEach(image => {
         modal.show();
     });
 });
+
+
+const audio = document.getElementById('background-music');
+const playButton = document.getElementById('play-music-btn');
+const stopButton = document.getElementById('stop-music-btn');
+
+// Tambahkan event listener untuk tombol play
+playButton.addEventListener('click', () => {
+    // Memutar musik
+    audio.play();
+    // Menyembunyikan tombol play dan menampilkan tombol stop
+    playButton.style.display = 'none';
+    stopButton.style.display = 'inline-block';
+});
+
+// Tambahkan event listener untuk tombol stop
+stopButton.addEventListener('click', () => {
+    // Menghentikan musik
+    audio.pause();
+    // Menyembunyikan tombol stop dan menampilkan tombol play
+    stopButton.style.display = 'none';
+    playButton.style.display = 'inline-block';
+});
